29731018819f9629fec90d6ed268bed41c774f8d
Arya Sreenivasan <aryasreenivasan14@gmail.com>
2018-05-21 16:56:37 +1000

    Created the project

---
+++ CollectionViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewTest
+//
+//  Created by Arya Sreenivasan on 21/5/18.
+//  Copyright © 2018 AryaSreenivasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
